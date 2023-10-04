65c96a77b9ea6265820eded0064e4af9cfd199a0
Jamal Mydeen <jamessies123@gmail.com>
2023-10-05 00:31:24 +0530

    Update prev.js

--- client/src/utils/prev.js
+++ client/src/utils/prev.js
@@ -4,7 +4,7 @@ const data=[
      date:"2023-10-2",
      categories:["webdev","Reactjs","nodejs"],
      author:"Jamal Mydeen",
-     excerts:"Why do you need to use React Fragments? What are the benefits of using fragments over divs? & Why not to use HTML elements?",
+     excerts:"Why do you need to use Nextjs? What are the benefits of using Nextjs over Reactjs?",
      github: "https://raw.githubusercontent.com/jamAL108/connect/main/client/src/blogs/nextjs.md",
      coverImage:"https://firebasestorage.googleapis.com/v0/b/blogs-8e84a.appspot.com/o/images%2FUntitled%20design%20(13).png?alt=media&token=425e916d-c093-48a0-bce9-b722d5332b5a&_gl=1*1oyvsnb*_ga*MTcxNDE5NzgzLjE2ODcwNzU0MTQ.*_ga_CW55HF8NVT*MTY5NjQzODE4MS4xOS4xLjE2OTY0Mzg2MjAuMjguMC4w"
    },{
