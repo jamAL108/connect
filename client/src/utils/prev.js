5c42ae3a6b2e79dff621f18f84b319a6348f391c
JamalMydeen <jamessies123@gmail.com>
2023-10-07 12:54:53 +0530

    .

--- client/src/utils/prev.js
+++ client/src/utils/prev.js
@@ -6,15 +6,15 @@ const data=[
      author:"Jamal Mydeen",
      excerts:"Why do you need to use React Fragments? What are the benefits of using fragments over divs? & Why not to use HTML elements?",
      github: "https://raw.githubusercontent.com/jamAL108/connect/main/client/src/blogs/nextjs.md",
-     coverImage:"https://firebasestorage.googleapis.com/v0/b/blogs-8e84a.appspot.com/o/images%2FUntitled%20design%20(13).png?alt=media&token=425e916d-c093-48a0-bce9-b722d5332b5a&_gl=1*1oyvsnb*_ga*MTcxNDE5NzgzLjE2ODcwNzU0MTQ.*_ga_CW55HF8NVT*MTY5NjQzODE4MS4xOS4xLjE2OTY0Mzg2MjAuMjguMC4w"
+     coverImage:"https://firebasestorage.googleapis.com/v0/b/blogs-8e84a.appspot.com/o/images%2Fpic1.png?alt=media&token=7127b29f-ae7f-453a-960b-ce988b08ed7d"
    },{
-    title:"A Deep dive into Advantages of Next.js compared to React.js",
-    date:"2023-10-2",
+    title:"What is Blockchain? and what are different types of server?",
+    date:"2023-10-7",
     categories:["webdev","Reactjs","nodejs"],
     author:"Jamal Mydeen",
     excerts:"Why do you need to use React Fragments? What are the benefits of using fragments over divs? & Why not to use HTML elements?",
     github: "https://raw.githubusercontent.com/jamAL108/connect/main/src/blogs/nextjs.md",
-    coverImage:"https://firebasestorage.googleapis.com/v0/b/blogs-8e84a.appspot.com/o/images%2FUntitled%20design%20(13).png?alt=media&token=425e916d-c093-48a0-bce9-b722d5332b5a&_gl=1*1oyvsnb*_ga*MTcxNDE5NzgzLjE2ODcwNzU0MTQ.*_ga_CW55HF8NVT*MTY5NjQzODE4MS4xOS4xLjE2OTY0Mzg2MjAuMjguMC4w"
+    coverImage:"https://firebasestorage.googleapis.com/v0/b/blogs-8e84a.appspot.com/o/images%2Fpic2.png?alt=media&token=efe94bfb-2777-4a47-9cfb-67e0fa3ddfb5"
   },{
     title:"A Deep dive into Advantages of Next.js compared to React.js",
     date:"2023-10-2",
